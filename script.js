7ad08cfe18183e59942eef562bc3f035de94606b
walterlee <walter.lee9@gmail.com>
2018-01-15 16:39:22 -0600

    added modulus and indexing accusation in the alert correctly

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
+var friend = ['Blue', 'Black', 'Red', 'Yellow', 'Green'];
+var weapon = ['bottle', 'knuckles', 'fist', 'boomerang', 'bomb', 'slingshot', 'bat', 'knife', 'wand', 'club', 'sword', 'shuriken', 'spear', 'whip', 'axe', 'katana', 'kunai', 'nunchuku', 'bo', 'sai'];
+var locations = ['sky', 'ocean', 'mountain', 'desert', 'forest', 'space', 'tundra', 'plains', 'hills', 'grassland'];
 
-for (var i = 1; i < 101; ++i) {
-    var friend = ['Blue', 'Black', 'Red', 'Yellow', 'Green'];
-    var weapon = ['bottle', 'knuckles', 'fist', 'boomerang', 'bomb', 'slingshot', 'bat', 'knife', 'wand', 'club', 'sword', 'shuriken', 'spear', 'whip', 'axe', 'katana', 'kunai', 'nunchuku', 'bo', 'sai'];
-    var locations = ['sky', 'ocean', 'mountain', 'desert', 'forest', 'space', 'tundra', 'plains', 'hills', 'grassland'];
+
+for (var index = 1; index < 101; index++) {
     var h3 = document.createElement('h3');
 
-    h3.innerText = 'Accusation ' + i;
+    h3.innerText = 'Accusation ' + index;
     document.body.appendChild(h3);
 
-    function randomFriend() {
-        return friend[Math.floor(Math.random()*friend.length)];
-    }
-
-    function randomWeapon() {
-        return weapon[Math.floor(Math.random()*weapon.length)];
-    }
-
-    function randomLocations() {
-        return locations[Math.floor(Math.random()*locations.length)];
-    }
-    
-    h3.addEventListener('click', function(){
-        alert('I accuse ' + randomFriend() + ', with the ' + randomWeapon() + ' in the ' + randomLocations() + '!');
-    });
+    h3.addEventListener('click', accusation(index));
 };
 
+function accusation(index) {
 
+    var friendName = friend[index % friend.length];
+    var weaponName = weapon[index % weapon.length];
+    var locationsName = locations[index % locations.length];
+
+    return function () {
+        alert('Accusation ' + index + ': I accuse ' + friendName +
+            ', with the ' + weaponName + ' in the ' + locationsName + '!');
+    }
+}
 
